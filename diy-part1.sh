63170f12f92baeed3ee4b83c3fc4397d9a10e64c
mslovecc <33970221+mslovecc@users.noreply.github.com>
2022-12-12 18:39:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ git fetch --tags
 git tag -l
  
 # Use OpenWrt 19.07.7 release
-git checkout v21.02.2
+git checkout v22.03.2
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # chmod +x $GITHUB_WORKSPACE/lede/copy-amd64.sh
